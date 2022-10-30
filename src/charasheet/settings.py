8196422f9e38bcc2144b29d3bc5012b06237c7b3
Gabriel Augendre <gabriel@augendre.info>
2022-10-30 10:56:10 +0100

    Add django browser reload for dev

--- src/charasheet/settings.py
+++ src/charasheet/settings.py
@@ -55,7 +55,9 @@ EXTERNAL_APPS = [
     "django_cleanup.apps.CleanupConfig",  # should be last: https://pypi.org/project/django-cleanup/
 ]
 if DEBUG_TOOLBAR:
-    EXTERNAL_APPS.append("debug_toolbar")
+    EXTERNAL_APPS.insert(-2, "debug_toolbar")
+if DEBUG:
+    EXTERNAL_APPS.insert(-2, "django_browser_reload")
 
 CUSTOM_APPS = [
     "whitenoise.runserver_nostatic",  # should be first
@@ -78,6 +80,8 @@ MIDDLEWARE = [
 ]
 if DEBUG_TOOLBAR:
     MIDDLEWARE.insert(0, "debug_toolbar.middleware.DebugToolbarMiddleware")
+if DEBUG:
+    MIDDLEWARE.append("django_browser_reload.middleware.BrowserReloadMiddleware")
 
 ROOT_URLCONF = "charasheet.urls"
 
